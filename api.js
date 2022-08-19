648d58b1281f08bcf788efaf68d2db7036004ba8
Sarah Zhang <15064710+sazhang@users.noreply.github.com>
2022-08-19 12:07:47 -0700

    connect wallet and follow user

--- api.js
+++ api.js
@@ -92,8 +92,123 @@ export const recommendedProfiles = `
 `;
 
 export const getProfileById = `
-query Profile($id: ProfileId!) {
-  profile(request: { profileId: $id }) {
+  query Profile($id: ProfileId!) {
+    profile(request: { profileId: $id }) {
+      id
+      name
+      bio
+      attributes {
+        displayType
+        traitType
+        key
+        value
+      }
+      followNftAddress
+      metadata
+      isDefault
+      picture {
+        ... on NftImage {
+          contractAddress
+          tokenId
+          uri
+          verified
+        }
+        ... on MediaSet {
+          original {
+            url
+            mimeType
+          }
+        }
+        __typename
+      }
+      handle
+      coverPicture {
+        ... on NftImage {
+          contractAddress
+          tokenId
+          uri
+          verified
+        }
+        ... on MediaSet {
+          original {
+            url
+            mimeType
+          }
+        }
+        __typename
+      }
+      ownedBy
+      dispatcher {
+        address
+        canUseRelay
+      }
+      stats {
+        totalFollowers
+        totalFollowing
+        totalPosts
+        totalComments
+        totalMirrors
+        totalPublications
+        totalCollects
+      }
+      followModule {
+        ... on FeeFollowModuleSettings {
+          type
+          amount {
+            asset {
+              symbol
+              name
+              decimals
+              address
+            }
+            value
+          }
+          recipient
+        }
+        ... on ProfileFollowModuleSettings {
+          type
+        }
+        ... on RevertFollowModuleSettings {
+          type
+        }
+      }
+    }
+  }
+`;
+
+export const getPublicationsById = `
+  query Publications ($id: ProfileId!) {
+    publications(request: {
+      profileId: $id,
+      publicationTypes: [POST, COMMENT, MIRROR],
+      limit: 10
+    }) {
+      items {
+        __typename 
+        ... on Post {
+          ...PostFields
+        }
+        ... on Comment {
+          ...CommentFields
+        }
+        ... on Mirror {
+          ...MirrorFields
+        }
+      }
+      pageInfo {
+        prev
+        next
+        totalCount
+      }
+    }
+  }
+
+  fragment MediaFields on Media {
+    url
+    mimeType
+  }
+
+  fragment ProfileFields on Profile {
     id
     name
     bio
@@ -103,9 +218,12 @@ query Profile($id: ProfileId!) {
       key
       value
     }
+    isFollowedByMe
+    isFollowing(who: null)
     followNftAddress
     metadata
     isDefault
+    handle
     picture {
       ... on NftImage {
         contractAddress
@@ -115,13 +233,10 @@ query Profile($id: ProfileId!) {
       }
       ... on MediaSet {
         original {
-          url
-          mimeType
+          ...MediaFields
         }
       }
-      __typename
     }
-    handle
     coverPicture {
       ... on NftImage {
         contractAddress
@@ -131,16 +246,13 @@ query Profile($id: ProfileId!) {
       }
       ... on MediaSet {
         original {
-          url
-          mimeType
+          ...MediaFields
         }
       }
-      __typename
     }
     ownedBy
     dispatcher {
       address
-      canUseRelay
     }
     stats {
       totalFollowers
@@ -156,8 +268,8 @@ query Profile($id: ProfileId!) {
         type
         amount {
           asset {
-            symbol
             name
+            symbol
             decimals
             address
           }
@@ -166,336 +278,224 @@ query Profile($id: ProfileId!) {
         recipient
       }
       ... on ProfileFollowModuleSettings {
-        type
+      type
       }
       ... on RevertFollowModuleSettings {
-        type
+      type
       }
     }
   }
-}`;
 
-
-
-export const getPublicationsById = `
-query Publications ($id: ProfileId!) {
-  publications(request: {
-    profileId: $id,
-    publicationTypes: [POST, COMMENT, MIRROR],
-    limit: 10
-  }) {
-    items {
-      __typename 
-      ... on Post {
-        ...PostFields
-      }
-      ... on Comment {
-        ...CommentFields
-      }
-      ... on Mirror {
-        ...MirrorFields
-      }
-    }
-    pageInfo {
-      prev
-      next
-      totalCount
-    }
+  fragment PublicationStatsFields on PublicationStats { 
+    totalAmountOfMirrors
+    totalAmountOfCollects
+    totalAmountOfComments
   }
-}
 
-fragment MediaFields on Media {
-  url
-  mimeType
-}
-
-fragment ProfileFields on Profile {
-  id
-  name
-  bio
-  attributes {
-     displayType
-     traitType
-     key
-     value
-  }
-  isFollowedByMe
-  isFollowing(who: null)
-  followNftAddress
-  metadata
-  isDefault
-  handle
-  picture {
-    ... on NftImage {
-      contractAddress
-      tokenId
-      uri
-      verified
-    }
-    ... on MediaSet {
+  fragment MetadataOutputFields on MetadataOutput {
+    name
+    description
+    content
+    media {
       original {
         ...MediaFields
       }
     }
-  }
-  coverPicture {
-    ... on NftImage {
-      contractAddress
-      tokenId
-      uri
-      verified
-    }
-    ... on MediaSet {
-      original {
-        ...MediaFields
-      }
+    attributes {
+      displayType
+      traitType
+      value
     }
   }
-  ownedBy
-  dispatcher {
+
+  fragment Erc20Fields on Erc20 {
+    name
+    symbol
+    decimals
     address
   }
-  stats {
-    totalFollowers
-    totalFollowing
-    totalPosts
-    totalComments
-    totalMirrors
-    totalPublications
-    totalCollects
-  }
-  followModule {
-    ... on FeeFollowModuleSettings {
+
+  fragment CollectModuleFields on CollectModule {
+    __typename
+    ... on FreeCollectModuleSettings {
+        type
+        followerOnly
+        contractAddress
+    }
+    ... on FeeCollectModuleSettings {
       type
       amount {
         asset {
-          name
-          symbol
-          decimals
-          address
+          ...Erc20Fields
         }
         value
       }
       recipient
+      referralFee
     }
-    ... on ProfileFollowModuleSettings {
-     type
-    }
-    ... on RevertFollowModuleSettings {
-     type
-    }
-  }
-}
-
-fragment PublicationStatsFields on PublicationStats { 
-  totalAmountOfMirrors
-  totalAmountOfCollects
-  totalAmountOfComments
-}
-
-fragment MetadataOutputFields on MetadataOutput {
-  name
-  description
-  content
-  media {
-    original {
-      ...MediaFields
-    }
-  }
-  attributes {
-    displayType
-    traitType
-    value
-  }
-}
-
-fragment Erc20Fields on Erc20 {
-  name
-  symbol
-  decimals
-  address
-}
-
-fragment CollectModuleFields on CollectModule {
-  __typename
-  ... on FreeCollectModuleSettings {
+    ... on LimitedFeeCollectModuleSettings {
       type
-      followerOnly
-      contractAddress
-  }
-  ... on FeeCollectModuleSettings {
-    type
-    amount {
-      asset {
-        ...Erc20Fields
-      }
-      value
-    }
-    recipient
-    referralFee
-  }
-  ... on LimitedFeeCollectModuleSettings {
-    type
-    collectLimit
-    amount {
-      asset {
-        ...Erc20Fields
-      }
-      value
-    }
-    recipient
-    referralFee
-  }
-  ... on LimitedTimedFeeCollectModuleSettings {
-    type
-    collectLimit
-    amount {
-      asset {
-        ...Erc20Fields
-      }
-      value
-    }
-    recipient
-    referralFee
-    endTimestamp
-  }
-  ... on RevertCollectModuleSettings {
-    type
-  }
-  ... on TimedFeeCollectModuleSettings {
-    type
-    amount {
-      asset {
-        ...Erc20Fields
-      }
-      value
-    }
-    recipient
-    referralFee
-    endTimestamp
-  }
-}
-
-fragment PostFields on Post {
-  id
-  profile {
-    ...ProfileFields
-  }
-  stats {
-    ...PublicationStatsFields
-  }
-  metadata {
-    ...MetadataOutputFields
-  }
-  createdAt
-  collectModule {
-    ...CollectModuleFields
-  }
-  referenceModule {
-    ... on FollowOnlyReferenceModuleSettings {
-      type
-    }
-  }
-  appId
-  hidden
-  reaction(request: null)
-  mirrors(by: null)
-  hasCollectedByMe
-}
-
-fragment MirrorBaseFields on Mirror {
-  id
-  profile {
-    ...ProfileFields
-  }
-  stats {
-    ...PublicationStatsFields
-  }
-  metadata {
-    ...MetadataOutputFields
-  }
-  createdAt
-  collectModule {
-    ...CollectModuleFields
-  }
-  referenceModule {
-    ... on FollowOnlyReferenceModuleSettings {
-      type
-    }
-  }
-  appId
-  hidden
-  reaction(request: null)
-  hasCollectedByMe
-}
-
-fragment MirrorFields on Mirror {
-  ...MirrorBaseFields
-  mirrorOf {
-   ... on Post {
-      ...PostFields          
-   }
-   ... on Comment {
-      ...CommentFields          
-   }
-  }
-}
-
-fragment CommentBaseFields on Comment {
-  id
-  profile {
-    ...ProfileFields
-  }
-  stats {
-    ...PublicationStatsFields
-  }
-  metadata {
-    ...MetadataOutputFields
-  }
-  createdAt
-  collectModule {
-    ...CollectModuleFields
-  }
-  referenceModule {
-    ... on FollowOnlyReferenceModuleSettings {
-      type
-    }
-  }
-  appId
-  hidden
-  reaction(request: null)
-  mirrors(by: null)
-  hasCollectedByMe
-}
-
-fragment CommentFields on Comment {
-  ...CommentBaseFields
-  mainPost {
-    ... on Post {
-      ...PostFields
-    }
-    ... on Mirror {
-      ...MirrorBaseFields
-      mirrorOf {
-        ... on Post {
-           ...PostFields          
+      collectLimit
+      amount {
+        asset {
+          ...Erc20Fields
         }
-        ... on Comment {
-           ...CommentMirrorOfFields        
+        value
+      }
+      recipient
+      referralFee
+    }
+    ... on LimitedTimedFeeCollectModuleSettings {
+      type
+      collectLimit
+      amount {
+        asset {
+          ...Erc20Fields
+        }
+        value
+      }
+      recipient
+      referralFee
+      endTimestamp
+    }
+    ... on RevertCollectModuleSettings {
+      type
+    }
+    ... on TimedFeeCollectModuleSettings {
+      type
+      amount {
+        asset {
+          ...Erc20Fields
+        }
+        value
+      }
+      recipient
+      referralFee
+      endTimestamp
+    }
+  }
+
+  fragment PostFields on Post {
+    id
+    profile {
+      ...ProfileFields
+    }
+    stats {
+      ...PublicationStatsFields
+    }
+    metadata {
+      ...MetadataOutputFields
+    }
+    createdAt
+    collectModule {
+      ...CollectModuleFields
+    }
+    referenceModule {
+      ... on FollowOnlyReferenceModuleSettings {
+        type
+      }
+    }
+    appId
+    hidden
+    reaction(request: null)
+    mirrors(by: null)
+    hasCollectedByMe
+  }
+
+  fragment MirrorBaseFields on Mirror {
+    id
+    profile {
+      ...ProfileFields
+    }
+    stats {
+      ...PublicationStatsFields
+    }
+    metadata {
+      ...MetadataOutputFields
+    }
+    createdAt
+    collectModule {
+      ...CollectModuleFields
+    }
+    referenceModule {
+      ... on FollowOnlyReferenceModuleSettings {
+        type
+      }
+    }
+    appId
+    hidden
+    reaction(request: null)
+    hasCollectedByMe
+  }
+
+  fragment MirrorFields on Mirror {
+    ...MirrorBaseFields
+    mirrorOf {
+    ... on Post {
+        ...PostFields          
+    }
+    ... on Comment {
+        ...CommentFields          
+    }
+    }
+  }
+
+  fragment CommentBaseFields on Comment {
+    id
+    profile {
+      ...ProfileFields
+    }
+    stats {
+      ...PublicationStatsFields
+    }
+    metadata {
+      ...MetadataOutputFields
+    }
+    createdAt
+    collectModule {
+      ...CollectModuleFields
+    }
+    referenceModule {
+      ... on FollowOnlyReferenceModuleSettings {
+        type
+      }
+    }
+    appId
+    hidden
+    reaction(request: null)
+    mirrors(by: null)
+    hasCollectedByMe
+  }
+
+  fragment CommentFields on Comment {
+    ...CommentBaseFields
+    mainPost {
+      ... on Post {
+        ...PostFields
+      }
+      ... on Mirror {
+        ...MirrorBaseFields
+        mirrorOf {
+          ... on Post {
+            ...PostFields          
+          }
+          ... on Comment {
+            ...CommentMirrorOfFields        
+          }
         }
       }
     }
   }
-}
 
-fragment CommentMirrorOfFields on Comment {
-  ...CommentBaseFields
-  mainPost {
-    ... on Post {
-      ...PostFields
-    }
-    ... on Mirror {
-       ...MirrorBaseFields
+  fragment CommentMirrorOfFields on Comment {
+    ...CommentBaseFields
+    mainPost {
+      ... on Post {
+        ...PostFields
+      }
+      ... on Mirror {
+        ...MirrorBaseFields
+      }
     }
   }
-}`
+`;
--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,65 +1,96 @@
-import { useRouter } from "next/router"
-import { useState, useEffect } from "react"
-import { client, getProfileById, getPublicationsById } from "../../api"
-import Image from "next/image"
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import { client, getProfileById, getPublicationsById } from "../../api";
+import Image from "next/image";
+import ABI from "../../abi.json";
+import { ethers } from "ethers";
 
-export default function Profile(){ 
-    const [profile, setProfile] = useState()
-    const [pubs, setPubs] = useState([])
-    const router = useRouter()
-    const { id } = router.query
+const CONTRACT_ADDRESS = "0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d";
 
-    useEffect(() => {
-        if(id){
-            fetchProfile()
-        }
-    }, [id])
+export default function Profile() {
+  const [profile, setProfile] = useState();
+  const [pubs, setPubs] = useState([]);
+  const router = useRouter();
+  const { id } = router.query;
 
-    async function fetchProfile(){
-        try{
-            const response = await client.query(getProfileById, { id }).toPromise()
-            console.log("PROFILE:", response)
-            setProfile(response.data.profile)
-
-            const publications = await client.query(getPublicationsById, { id }).toPromise()
-            console.log("PUBS!", publications)
-            setPubs(publications.data.publications.items)
-        } catch(error){
-            console.log("ERROR:", error)
-        }
+  useEffect(() => {
+    if (id) {
+      fetchProfile();
     }
+  }, [id]);
 
-    return (
+  async function fetchProfile() {
+    try {
+      const response = await client.query(getProfileById, { id }).toPromise();
+      console.log("PROFILE:", response);
+      setProfile(response.data.profile);
+
+      const publications = await client
+        .query(getPublicationsById, { id })
+        .toPromise();
+      console.log("PUBS!", publications);
+      setPubs(publications.data.publications.items);
+    } catch (error) {
+      console.log("ERROR:", error);
+    }
+  }
+
+  async function connectWallet() {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    console.log("accounts: ", accounts);
+  }
+
+  async function followUser() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+
+    const contract = new ethers.Contract(address, ABI, signer);
+
+    try {
+      const tx = await contract.follow([id], [0x0]);
+      await tx.wait();
+      console.log("Followed user successfully");
+    } catch (err) {
+      console.log("Failed to follow user due to", err);
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={connectWallet}>Connect</button>
+      {profile && (
         <div>
-            {profile && <div>
-                {profile.picture && profile.picture.original ? (
-                <Image
-                  src={profile.picture.original.url}
-                  width="52px"
-                  height="52px"
-                  alt={profile.handle}
-                />
-              ) : (
-                <div className="bg-gray-100" />
-              )}
-              <p>{profile.handle}</p>
-              <p>{profile.bio}</p>
-              <p>Followers: {profile.stats.totalFollowers}</p>
-              <p>Following: {profile.stats.totalFollowing}</p>
-                </div>}
-
-                {pubs.length > 0 && 
-                <div className="py-4">
-                 {pubs.map((p, index) => (
-                 <div key={p.id}>
-                    <p className="font-bold">{p.__typename}</p>
-                    <p>{p.metadata.content}</p>
-                    <p>{p.metadata.name}</p>
-                </div>
-                 ))}
-                   
-                </div>}
+          {profile.picture && profile.picture.original ? (
+            <Image
+              src={profile.picture.original.url}
+              width="52px"
+              height="52px"
+              alt={profile.handle}
+            />
+          ) : (
+            <div className="bg-gray-100" />
+          )}
+          <p>{profile.handle}</p>
+          <p>{profile.bio}</p>
+          <p>Followers: {profile.stats.totalFollowers}</p>
+          <p>Following: {profile.stats.totalFollowing}</p>
+          <button onClick={followUser}>Follow</button>
         </div>
-    )
-}
+      )}
 
+      {pubs.length > 0 && (
+        <div className="py-4">
+          {pubs.map((p, index) => (
+            <div key={p.id}>
+              <p className="font-bold">{p.__typename}</p>
+              <p>{p.metadata.content}</p>
+              <p>{p.metadata.name}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
